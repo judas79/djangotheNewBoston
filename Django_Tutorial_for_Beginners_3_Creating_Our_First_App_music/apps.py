feceb239234a5af2a2fff3c2a0eac6f06f4f7765
Roddy <robotrods@gmail.com>
2020-02-09 23:26:30 -0500

    Lesson 3 directory for music app using the lessons name for the app

---
+++ Django_Tutorial_for_Beginners_3_Creating_Our_First_App_music/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoTutorialForBeginners3CreatingOurFirstAppMusicConfig(AppConfig):
+    name = 'Django_Tutorial_for_Beginners_3_Creating_Our_First_App_music'
