04002887961aeae16a83789984739b41a2035d00
Roddy <robotrods@gmail.com>
2020-02-18 16:06:42 -0500

    Lesson 12 commited and pushed

--- music/views.py
+++ music/views.py
@@ -7,3 +7,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse('<h1>response text goes in here</h1>')
 
+#def detail(request, album_id):
+    #return HttpResponse('<h2>Details for Album ID:  ', str(album_id), '</h2>')
+
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Detail for Album ID: %s. " % album_id)
+
