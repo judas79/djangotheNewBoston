b6f2aa57d3510a7023ec0d415bdc0e8e832c200c
Roddy <robotrods@gmail.com>
2020-02-19 16:27:31 -0500

    Lesson 14 commited and pushed

--- music/views.py
+++ music/views.py
@@ -1,28 +1,39 @@
 # from django.shortcuts import render
-
 # Create your views here.
 
+#added for lesson 13
+from django.template import loader
 from django.http import HttpResponse
 from . models import Album
+
+# From lesson 13
+'''
+def index(request):
+    # use Album.objects.all() to get all albums
+    all_albums = Album.objects.all()
+    # make html a string
+    html = ''
+    # iterate through all albums
+    for album in all_albums:
+        # get urls
+        url = '/music/' + str(album.id) + '/'
+        # convert url to html for a website link
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+'''
 
 def index(request):
 
     # use Album.objects.all() to get all albums
     all_albums = Album.objects.all()
+    # load our template contained in index.html
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+        }
+    return HttpResponse(template.render(context, request))
 
-    # make html a string
-    html = ''
 
-    # iterate through all albums
-    for album in all_albums:
-
-        # get urls
-        url = '/music/' + str(album.id) + '/'
-
-        # convert url to html for a website link
-        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
-
-    return HttpResponse(html)
 
     #From an earlier lesson
     #return HttpResponse('<h1>response text goes in here</h1>')
