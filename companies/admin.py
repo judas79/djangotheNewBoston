7ae5eb01817a2ba7e5cfe02c9430a9fda0b2b998
Roddy <robotrods@gmail.com>
2020-04-14 18:36:18 -0400

    Lesson 38 commit and push

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+# L38 import the class Stock from the model.py file we created
+from .models import Stock
 
 # Register your models here.
+
+# add ability to add or delete Stocks, from within sites admin panel
+admin.site.register(Stock)
+
--- companies/models.py
+++ companies/models.py
@@ -3,4 +3,14 @@ from django.db import models
 # Create your models here.
 # L37 inherit from import models
 class Stock(models.Model):
-    #
+
+    # L38 field attribute for company names, max. amount of characters
+    ticker = models.CharField(unique=True, max_length=10)
+    open = models.FloatField(max_length=20)
+    # more accurate when rounding numbers than Float, must have decimal_places field to use with decimal points
+    close = models.DecimalField(max_digits=5, decimal_places=3)
+    volume = models.IntegerField()
+
+    # L38 string representation of the ticker field, above
+    def __str__(self):
+        return self.ticker
